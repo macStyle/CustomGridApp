2ad3a3a68dc96e9d5aa1e97273c519fd08b13f56
med <theoutsider009@gmail.com>
2021-12-28 12:34:16 +0100

    Initial Commit

---
+++ CustomGridApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomGridApp
+//
+//  Created by Adam Jemni on 12/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomGridApp/CustomGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridAppApp.swift
+//  CustomGridApp
+//
+//  Created by Adam Jemni on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
